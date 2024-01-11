c9c98a5b3bf42f0ec492ddcd26447ade4031573c
Filip Cecelja <filip.cecelja@fer.hr>
2024-01-11 12:46:54 +0100

    Added IamPoor App Challenge

---
+++ Section3/IamPoor/IamPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamPoor
+//
+//  Created by Filip Cecelja on 11.01.2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
